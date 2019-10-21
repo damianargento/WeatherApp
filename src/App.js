2eabecc22a9dfa657294770dc6065db173196518
Argento <damian.argento@yrbrands.com>
2019-10-21 18:05:36 -0300

    First commit in this Weather App. Adding The API calls and functions

--- src/App.js
+++ src/App.js
@@ -2,25 +2,59 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class TimeApp extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			current: {},
+			ubicacion: {},
+			isLoading:false,
+			userQuery: "Buenos Aires"
+			}
+		}
+
+    componentDidMount() {
+		this.setState({isLoading:true})
+		fetch(API + this.state.userQuery)
+		.then(response => response.json())
+		.then(data => this.setState({ current: data.current, ubicacion: data.location, isLoading: false, }))
+}
+search = event => {
+	this.componentDidMount();
+}
+handleChange = event => {
+	this.setState({ userQuery: event.target.value });
+  };
+
+	render() {
+		const { current } = this.state;
+		const { ubicacion } = this.state;
+		const { isLoading } = this.state;
+		return (
+				isLoading ? <div>Cargando...</div> : 
+				<div>
+					
+				<div><img src={current.weather_icons} alt=""/></div>
+				<div>{ubicacion.name}</div>
+				<div>{current.temperature} °C</div>
+				<input value={this.state.userQuery} onChange={this.handleChange}></input>
+				<button onClick={this.search}>Search</button>
+				</div>
+			)
+		}
+	}
+const App = () => (
+<div className="generalDiv">
+	<header className="headerStyle">
+		<nav className="navStyle">
+		    <div className="menuStyle hide-mob">
+			</div>
+		</nav>
+	</header>
+	<section className="App">
+		<TimeApp/>
+	</section>
+</div>
+);
 export default App;
