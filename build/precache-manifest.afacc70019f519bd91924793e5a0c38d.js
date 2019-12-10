4913cd5cb647b8d5989a5bbee11ce48f063146fa
damianargento <47644346+damianargento@users.noreply.github.com>
2019-12-10 13:51:06 -0300

    Added build

---
+++ build/precache-manifest.afacc70019f519bd91924793e5a0c38d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b8cd5e3990070c26070b541231ca13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcb1e45394a5d5e80c2a",
+    "url": "/weatherApp/static/css/main.093edf76.chunk.css"
+  },
+  {
+    "revision": "63f753597817df6a8580",
+    "url": "/weatherApp/static/js/2.21288323.chunk.js"
+  },
+  {
+    "revision": "dcb1e45394a5d5e80c2a",
+    "url": "/weatherApp/static/js/main.5c0f2eef.chunk.js"
+  },
+  {
+    "revision": "53ed4ce02e8cc24018f1",
+    "url": "/weatherApp/static/js/runtime-main.85e76c8f.js"
+  }
+]);
